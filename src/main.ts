a88af71bbb164843f7e908addd3333dee01bfae2
PanDymitr <chadovich.d@gmail.com>
2023-12-28 09:03:58 +0200

    feat(app): enable validation

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,27 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableShutdownHooks();
+  app.setGlobalPrefix('api');
+  app.enableCors();
+
+  app.enableVersioning({
+    defaultVersion: '1',
+    type: VersioningType.URI,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      forbidUnknownValues: true,
+      whitelist: true,
+    }),
+  );
 
   await app.listen(process.env.PORT || 5002);
 }
+
 bootstrap();
